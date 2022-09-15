abed616270adf52bc87d5a6db65e47c6b25e9ad2
Sahil <112234265+amicable-sahil@users.noreply.github.com>
2022-09-15 17:03:15 +0530

    Update Project_1.1.py

--- Project_1.1.py
+++ Project_1.1.py
@@ -4,26 +4,27 @@ print("Instructions:")
 print("-> Make a meaningful word with the letters giving in following question.")
 print("-> Every correct answer has 1 score and every wrong answer has -1 score.", end="\n\n")
 
-d1 = {"father": "RAEHTF", "break": "KABRE", "country": "CYROTNU", "green": "RENEG", "aeroplane":"OAERELANP"}
-d2 = {"programme": "EGMORMARP", "orange": "AENROG", "pineapple": "LPEANIPEP", "leboratory":"YTAOBREOLR", "welcome":"MLOECWE"}
-d3 = {"student": "TUTNDSE", "horse": "ROSHE", "peacock": "CAOECPK", "crocodile":"EDOOIRCCL", "pleasure":"UESLARPE"}
-d4 = {"tiger": "RGIET", "violate": "TOAILVE", "vegetable": "LETGAEBVE", "mountain":"NUTONAMI", "raddish":"HDASRID"}
+dict1 = {"father": "RAEHTF", "break": "KABRE", "country": "CYROTNU", "green": "RENEG", "aeroplane":"OAERELANP"}
+dict2 = {"programme": "EGMORMARP", "orange": "AENROG", "pineapple": "LPEANIPEP", "leboratory":"YTAOBREOLR", "welcome":"MLOECWE"}
+dict3 = {"student": "TUTNDSE", "horse": "ROSHE", "peacock": "CAOECPK", "crocodile":"EDOOIRCCL", "pleasure":"UESLARPE"}
+dict4 = {"tiger": "RGIET", "violate": "TOAILVE", "vegetable": "LETGAEBVE", "mountain":"NUTONAMI", "raddish":"HDASRID"}
 
 
-myList = [d1, d2, d3, d4]
+myList = [dict1, dict2, dict3, dict4]
 
 
-def fun(dictonary):
+def wordPuzzle(dictonary):
     i = 1
     score = 0
-    for k, v in dictonary.items():
-        print("Q{}. Enter the correct word for [ {} ]".format(i, v))
+    for dict_key, dict_value in dictonary.items():
+        print(
+            "Q{}. Re-aarange and enter the correct word for [ {} ]".format(i, dict_value))
         ans = input()
-        if ans.lower() == k:
-            print("You are genius")
+        if ans.lower() == dict_key:
+            print("Wow ! You are genius")
             score +=1
         else:
-            print("Tum gadhe ho")
+            print("Oops ! You are wrong")
             score -=1
         i +=1
         print()
@@ -32,8 +33,8 @@ def fun(dictonary):
     print()
     
  
-for d in myList:
-    fun(d)   
+for data in myList:
+    wordPuzzle(data)
     play = input("Do you want to play more (y/n)")
     print()
     if play == "y" or play == "Y":
